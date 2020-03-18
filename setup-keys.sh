eafbb581ab59c3ebe1dd3ea0866bfd128442521c
JaanTaponen <jaan.taponen@hotmail.com>
2020-03-18 14:58:51 +0200

    p12 key generation

--- setup-keys.sh
+++ setup-keys.sh
@@ -1,12 +1,40 @@
 #!/bin/bash
 if [[ $# -ne 2 ]] ; then
-    echo 'Please give 2 commandline parameters: [filename.p12, password]'
-    exit 1
+    echo 'No parameters detected.'
+    while true; do
+        read -p "Create .p12 certificate?[Yy/Nn] " yn </dev/tty
+        case $yn in
+        [Yy]*)
+            openssl req -newkey rsa:2048 -nodes -keyout key.pem -x509 -days 365 -out certificate.pem
+            openssl x509 -text -noout -in certificate.pem
+            echo -n 'Give export password: '
+            read -s password
+            filename='user_a.p12'
+            openssl pkcs12 -inkey key.pem -in certificate.pem -export -out $filename -passout pass:$password
+            rm certificate.pem && rm key.pem
+            break
+            ;;
+        [Nn]*) 
+            echo 'Please give 2 commandline parameters: [filename.p12, password]'
+            exit 1
+            break 
+            ;;
+        *) echo "Please answer yes or no." ;;
+        esac
+    done
+    else
+        filename=$1
+        password=$2
 fi
 
-filename=$1
-password=$2
+clear
+echo '-----------------------------------'
+echo ''
+echo 'DSS init script (c) Aalto Smartcom'
+echo ''
+echo '-----------------------------------'
 
+# Checks for the certificate existion.
 if [ -f "$filename" ]; then
     echo "$filename OK!"
 else 
@@ -20,8 +48,10 @@ else
     echo "Wrong password?"
 fi
 
+echo 'Generating dss.properties file...'
 cp template.properties dss.properties
 perl -pi -e "s/user\_a\_rsa\.p12/${filename}/g" ./dss.properties
 perl -pi -e "s/password\_user/${password}/g" ./dss.properties
 yes | cp -rf ./dss.properties ./dss-demo-webapp/src/main/resources/dss.properties
 yes | cp -rf $filename ./dss-demo-webapp/src/main/resources/$filename
+echo 'Script completed succesfully.'
